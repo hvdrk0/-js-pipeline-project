bdac154815389e99cdbe5d20d3cea151f15f638b
hvdrk0 <hvdrk0@gmail.com>
2024-07-08 15:57:02 +0900

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,10 +1,12 @@
 def buildApp() {
-    echo 'building the applications...'
+  echo 'building the applications...'
 }
 def testApp() {
-    echo 'testing the applications...'
+  echo 'testing the applications...'
+  echo 'Replay'
 }
 def deployApp() {
-    echo 'deploying the application...'
-    echo "deploying version ${params.VERSION}"
+  echo 'deploying the applicaiton...'
+  echo "deploying version ${params.VERSION}"
 }
+return this
